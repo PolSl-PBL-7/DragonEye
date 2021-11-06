d7b34bd64ba78150106b8d92aff6768d564d7090
Krzysztof Kramarz <40365532+Fakser@users.noreply.github.com>
2021-11-06 15:58:03 +0100

    Create module for data processing

---
+++ DataProc/load.py
@@ -0,0 +1,24 @@
+import mypy
+
+import skvideo.io as sk 
+import wandb
+
+import numpy as np
+
+
+class VideoLoader(object):
+    filepath: str
+
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+    
+    def load(self) -> np.array:
+        pass
+
+class LocalLoader(VideoLoader):
+    def load(self) -> np.array:
+        return sk.vread(self.filepath)
+
+class WANDBLoader(VideoLoader):
+    def load(self) -> np.array:
+        pass
