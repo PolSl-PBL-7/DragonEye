59dc5fe3fccd435283e37c5792225c8d1343aad6
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-11-21 18:09:21 +0100

    fix: cuda install

--- scripts/install-cuda.sh
+++ scripts/install-cuda.sh
@@ -9,8 +9,10 @@ else
     SUDO="sudo"
 fi
 
+$SUDO apt-get update
+$SUDO apt-get install -y --no-install-recommends software-properties-common
 $SUDO apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/7fa2af80.pub
-$SUDO add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/"
+$SUDO add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /"
 $SUDO add-apt-repository contrib
 $SUDO apt-get update
 $SUDO apt-get install -y cuda
