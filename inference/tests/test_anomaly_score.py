7daeaf849d6ef24cc32531fa619f72bd43d8bd39
Krzysztof Kramarz <40365532+Fakser@users.noreply.github.com>
2021-11-29 23:02:50 +0100

    feat: added test for anomaly score

---
+++ inference/tests/test_anomaly_score.py
@@ -0,0 +1,10 @@
+from inference.anomaly_score import heuristic_anomaly_score
+import tensorflow as tf
+
+def test_heuristic_anomaly_score():
+    true = tf.random.normal((32,10,227,227,3))
+    pred = tf.random.normal((32,10,227,227,3))
+
+    scores = heuristic_anomaly_score(true, pred)
+
+    assert scores.shape, (32,1)
