93da2e568bcb7f72c963a87e009bce8c04e10ace
arkadiusz-czerwinski <arek.pawel.czerwinski@gmail.com>
2021-11-29 20:36:59 +0100

    fixe: fixed conflicts with main branch

--- data/processing/process.py
+++ data/processing/process.py
@@ -1,4 +1,4 @@
-from typing import Tuple, NamedTuple, Optional
+from typing import Tuple, NamedTuple
 from tensorflow.python.data.ops.dataset_ops import BatchDataset
 
 import numpy as np
@@ -9,7 +9,6 @@ class ProcessorConfig(NamedTuple):
     shape: Tuple[int, int] = (227, 227)
     time_window: int = 10
     batch_size: int = 32
-    padding: str = 'zero'
 
 
 class VideoProcessor:
@@ -20,19 +19,17 @@ class VideoProcessor:
 
     def __init__(self, config: ProcessorConfig):
         """
-        Object used to adjust shape, create time windows and batch data. 
+        Object used to adjust shape, create time windows and batch data.
         In the future it may be extended with different masks/filters.
 
         Args:
             shape (tuple, optional): Width and height that video should be adjusted to. Defaults to (320, 320).
             time_window (int, optional): Nuber of frames per example. Defaults to 10.
             batch_size (int, optional): Number of examples/time windows per batch of data. Defaults to 128.
-            padding (str, optional): Version of padding that should be used on time dimension. Options: 'zero'; 'none'. Defaults to 'zero'.
         """
-        self.shape = config.shape
-        self.time_window = config.time_window
-        self.batch_size = config.batch_size
-        self.padding = config.padding
+        self.shape=config.shape
+        self.time_window=config.time_window
+        self.batch_size=config.batch_size
 
     def __call__(self, vid: np.ndarray) -> BatchDataset:
         """Method for applying preprocessing per recording.
@@ -44,6 +41,5 @@ class VideoProcessor:
             BatchDataset: Tensorflow Dataset
         """
         vid_resized = tf.image.resize(vid, self.shape)
-        vid_windowed_and_batched = tf.keras.utils.timeseries_dataset_from_array(vid_resized, None, self.time_window, 1,
-                                                                                1, self.batch_size)
+        vid_windowed_and_batched = tf.keras.utils.timeseries_dataset_from_array(vid_resized, None, self.time_window, 1, 1, self.batch_size)
         return vid_windowed_and_batched
