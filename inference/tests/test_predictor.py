a9c7dd52122f33cd9eedf7ccab2c8618d5a4f52f
damiankucharski <c.damiankucharski@gmail.com>
2021-11-29 23:30:43 +0000

    Fail test if no gpu

--- inference/tests/test_predictor.py
+++ inference/tests/test_predictor.py
@@ -1,3 +1,5 @@
+import pytest
+
 import tensorflow as tf
 from pathlib import Path
 
@@ -25,7 +27,8 @@ def test_full_experiment():
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
-
+    else:
+        pytest.fail("No gpu")
     # dataset preparation
     source_config = SourceConfig(batch_size=8, fps = 5)
     source = LocalVideoSource(source_config)
