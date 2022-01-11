718d4448faf19927c25930eb7790d38a4f2edb35
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2022-01-11 15:34:25 +0100

    feat: added better loading of config to sweep

--- utils/sweep.py
+++ utils/sweep.py
@@ -1,14 +1,15 @@
 import wandb
 import os.path as path
 import pprint
-import sys
 
 from pipelines import pipeline_runner as pr
+from utils.file_loaders import Json
 
 
 # Configuration
 PROJECT = 'testing-sweep'
 NUM_RUNS = 5
+CONFIG_TEMPLATE_LOCATION = path.join('.', 'pipelines', 'configs', 'training_params.json')
 
 sweep_config = {
     'name': 'test-sweep',
@@ -30,20 +31,41 @@ sweep_config = {
 }
 
 def train_sweep(config_defaults = None) -> None:
-    pprint.pprint(wandb.init())
+    wandb.init()
     config = wandb.config
-    config_dict = pr.build_config_dict(path.join('.', 'pipelines', 'configs'))
+
+    if path.isfile(CONFIG_TEMPLATE_LOCATION):
+        config_dict = Json.load(CONFIG_TEMPLATE_LOCATION)
+    else:
+        config_dict = pr.build_config_dict(CONFIG_TEMPLATE_LOCATION)
 
     pipeline = pr.get_pipeline_by_type(pr.PipelineType.training_pipeline)
 
-    config_dict['training_params']['source_arams']['fps'] = config['fps']
-    config_dict['training_params']['processor_params']['batch_size'] = config['batch_size']
-    config_dict['training_params']['processor_params']['time_window'] = config['time_window']
-    config_dict['training_params']['training_params']['epochs'] = config["epochs"]
+    print(10*'=', 'TEMPLATE', 10*'=')
+    pprint.pprint(config_dict)
+    print(10*'=', 'EOF', 10*'=')
+
+    try:
+        config_dict['source_params']['fps'] = config['fps']
+        config_dict['processor_params']['batch_size'] = config['batch_size']
+        config_dict['processor_params']['time_window'] = config['time_window']
+        config_dict['training_params']['epochs'] = config["epochs"]
+    except KeyError:
+        config_dict['training_params']['source_params']['fps'] = config['fps']
+        config_dict['training_params']['processor_params']['batch_size'] = config['batch_size']
+        config_dict['training_params']['processor_params']['time_window'] = config['time_window']
+        config_dict['training_params']['training_params']['epochs'] = config["epochs"]
+
+    print(10*'=', 'RECONFIGURED', 10*'=')
+    pprint.pprint(config_dict)
+    print(10*'=', 'EOF', 10*'=')
 
     pipeline(**config_dict)
 
 if __name__ == '__main__':
+    if not path.exists(CONFIG_TEMPLATE_LOCATION):
+        raise ValueError('given config file does not')
+
     wandb.login()
     sweep_id = wandb.sweep(sweep_config, project=PROJECT)
     wandb.agent(sweep_id, function=train_sweep, count=NUM_RUNS)
