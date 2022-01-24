fix: anomaly score calculation Co-authored-by: Damian Kucharski <c.DamianKucharski@gmail.com> Co-authored-by: Arkadiusz Czerwiński <arkadiusz-czerwinski@users.noreply.github.com>

inference/anomaly_score.py
@@ -3,25 +3,23 @@ from abc import ABC, abstractmethod
 
 import tensorflow as tf
 
-
 def mse_standarized(input, output):
-    abnormality_scores = tf.math.sqrt(tf.math.reduce_sum(
-        tf.math.square(input - output), axis=(2, 3, 4)))
-    minimum, maximum = tf.math.reduce_min(
-        abnormality_scores), tf.math.reduce_max(abnormality_scores)
-    abnormality_scores = tf.math.divide(
-        tf.math.subtract(abnormality_scores, minimum), maximum)
-    return abnormality_scores
+    input = input[:,-1,:,:,:]
+    output = output[:,-1,:,:,:]
+    abnormality_scores = tf.math.reduce_sum(tf.math.abs(input - output), axis=(1,2,3))/(input.shape[1]*input.shape[2]*input.shape[3])
+    return tf.reshape(abnormality_scores, (abnormality_scores.shape[0], 1))
 
 
 def peak_signal_noise_ratio(input, output):
+    input = input[:,-1,:,:,:]
+    output = output[:,-1,:,:,:]
     mse = tf.math.sqrt(
         tf.math.reduce_sum(
             tf.math.square(
                 input - output),
-            axis=(2, 3, 4)
+            axis=(1, 2, 3)
         )
-    )
+    )/input.shape[1]*input.shape[2]*input.shape[3]
     maximum = tf.math.reduce_max(mse)
     abnormality_scores = 10 * tf.math.log(
         tf.math.divide(
@@ -29,7 +27,7 @@ def peak_signal_noise_ratio(input, output):
             mse
         )
     )
-    return abnormality_scores
+    return tf.reshape(abnormality_scores, (abnormality_scores.shape[0], 1))
 
 
 heuristics = {